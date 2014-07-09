45feeca9294ca359c8cbbf5f5144fe121fda581a
Prime624 <bcp7bcp@yahoo.com>
2014-07-09 12:48:17 -0700

    Server-Client version mostly works now

--- display.py
+++ display.py
@@ -1,44 +1,48 @@
 import socket,pickle # Needed to recieve image
-import pygame as pg,sys,os,time # Needed to display image.
+import pygame as pg,sys,os,time # Needed to display image/time.
 
-s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect(('',55555))
-#s.send('bg')
-#backgroundPic=s.recv(1024)
-s.send('pointer')
-aaa=s.recv(35840)
-bbb=pickle.loads(aaa)
-pointerPic=pg.surfarray.make_surface(bbb)
-s.close()
-#Sets up pygame and displays image/time.
-pg.init()
-pg.mouse.set_visible(False)
-fpsClock = pg.time.Clock()
-font = pg.font.Font('freesansbold.ttf',26)
-mouseP=(0,0)
-point=(5000,5000)
-seconds=False
-#window = pg.display.set_mode((backgroundPic.get_rect().size[0],backgroundPic.get_rect().size[1]))
-window = pg.display.set_mode((500,500))#DELETE_ME
-while 1:
-	#window.blit(backgroundPic,(0,0))
-	window.blit(pointerPic,mouseP)
-	if seconds:
-		msgObj=font.render("It is {}".format(time.strftime("%H:%M:%S")), 1, pg.Color(0,0,0))
-	else:
-		msgObj=font.render("It is {}".format(time.strftime("%H:%M")), 1, pg.Color(0,0,0))
-	window.blit(msgObj,point)
-	for event in pg.event.get():
-		if event.type == pg.QUIT:
-			sys.exit()
-		elif event.type == pg.MOUSEMOTION:
-			mouseP = event.pos
-		elif event.type == pg.MOUSEBUTTONUP:
-			if event.button in (1,2):
-				point = event.pos
-			elif event.button==3:
-				point=(5000,5000)
-			else:
-				seconds=not seconds
-	pg.display.update()
-	fpsClock.tick(60)
+def main():##!!!!!! Need to transmit data over socket in chunks.
+	s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+	s.connect(('',55556))
+	#s.send('bg')
+	#bps=s.recv(87040)
+	#backgroundPic=pg.image.fromstring(bps[bps.index('QQQQQ')+5:],(int(bps[1:bps.index(',')]),int(bps[bps.index(' ')+1:bps.index(')')])),"RGBA")
+	s.send('pointer')
+	ps=s.recv(12288)
+	pointerPic=pg.image.fromstring(ps[ps.index('QQQQQ')+5:],(int(ps[1:ps.index(',')]),int(ps[ps.index(' ')+1:ps.index(')')])),"RGBA")
+	s.close()
+	#Sets up pygame and displays image/time.
+	pg.init()
+	pg.mouse.set_visible(False)
+	fpsClock = pg.time.Clock()
+	font = pg.font.Font('freesansbold.ttf',26)
+	mouseP=(0,0)
+	point=(5000,5000)
+	seconds=False
+	#window = pg.display.set_mode((backgroundPic.get_rect().size[0],backgroundPic.get_rect().size[1]))
+	window = pg.display.set_mode((500,500))#DELETE_ME
+	while 1:
+		window.blit(backgroundPic,(0,0))
+		window.blit(pointerPic,mouseP)
+		if seconds:
+			msgObj=font.render("It is {}".format(time.strftime("%H:%M:%S")), 1, pg.Color(0,0,0))
+		else:
+			msgObj=font.render("It is {}".format(time.strftime("%H:%M")), 1, pg.Color(0,0,0))
+		window.blit(msgObj,point)
+		for event in pg.event.get():
+			if event.type == pg.QUIT:
+				sys.exit()
+			elif event.type == pg.MOUSEMOTION:
+				mouseP = event.pos
+			elif event.type == pg.MOUSEBUTTONUP:
+				if event.button in (1,2):
+					point = event.pos
+				elif event.button==3:
+					point=(5000,5000)
+				else:
+					seconds=not seconds
+		pg.display.update()
+		fpsClock.tick(60)
+
+if __name__=='__main__':
+	main()
--- getter.py
+++ getter.py
@@ -1,31 +1,40 @@
 import urllib as ul,urllib2 as ul2 # Needed to download image from internet.
-import pygame as pg,socket,os,pickle # Needed to load and send image to display.
+import pygame as pg,socket,os # Needed to load and send image to display.
+import sys#DELETE_ME
 
-#Downloads sample image to temporary location.
-bgr=ul.urlretrieve('https://www.dropbox.com/s/2iz8c2f5pbdrh6d/aaa.png?dl=1','tmpImage.png')
-#Loads image into Pygame.
-bg=pg.image.load('tmpImage.png')
-#Deletes temporary image.
-os.remove('tmpImage.png')
-#Repeat above steps for second image.
-pointerr=ul.urlretrieve('https://www.dropbox.com/s/z4w5sgaueirxpsa/bbb.png?dl=1','tmpImage2.png')
-pointer=pg.image.load('tmpImage2.png')
-os.remove('tmpImage2.png')
-print 'Images downloaded successfully!'
+def main():
+	print 'Downloading images...'
+	#Downloads sample image to temporary location.
+	bgr=ul.urlretrieve('https://www.dropbox.com/s/2iz8c2f5pbdrh6d/aaa.png?dl=1','tmpImage.png')
+	#Loads image into Pygame.
+	bg=pg.image.load('tmpImage.png')
+	#Deletes temporary image.
+	os.remove('tmpImage.png')
+	#Repeat above steps for second image.
+	pointerr=ul.urlretrieve('https://www.dropbox.com/s/z4w5sgaueirxpsa/bbb.png?dl=1','tmpImage2.png')
+	pointer=pg.image.load('tmpImage2.png')
+	os.remove('tmpImage2.png')
+	print 'Images downloaded successfully!'
+	
+	#Sets up socket(server) and listens/accepts 1 client.
+	s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+	s.bind(('',55556))
+	s.listen(1)
+	conn,addr=s.accept()
+	while 1:
+		#Listens for data.
+		data=conn.recv(1024)
+		if not data: break
+		if data:
+			#Sends requested image over socket.
+			if data=='pointer':
+				size=pointer.get_size()
+				conn.send("{}QQQQQ{}".format(size,pg.image.tostring(pointer,"RGBA")))
+			elif data=='bg':
+				print sys.getsizeof(pg.image.tostring(bg,"RGBA"))
+				size=bg.get_size()
+				conn.send("{}QQQQQ{}".format(size,pg.image.tostring(bg,"RGBA")))
+	conn.close()
 
-#Sets up socket(server) and listens/accepts 1 client.
-s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.bind(('',55555))
-s.listen(1)
-conn,addr=s.accept()
-while 1:
-	#Listens for data.
-	data=conn.recv(1024)
-	if not data: break
-	if data:
-		#Sends requested image over socket.
-		if data=='pointer':
-			conn.send(pickle.dumps(pg.surfarray.array3d(pointer)))
-		elif data=='bg':
-			conn.send(pickle.dumps(pg.surfarray.array3d(bg)))
-conn.close()
+if __name__=='__main__':
+	main()
