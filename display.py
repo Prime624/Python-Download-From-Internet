7668e60397aea3ab1c4aabe5b49d86a3f6bd1e17
Prime624 <bcp7bcp@yahoo.com>
2014-07-09 14:22:34 -0700

    Finished Server-Client version

--- display.py
+++ display.py
@@ -1,14 +1,28 @@
-import socket,pickle # Needed to recieve image
+import socket # Needed to recieve image
 import pygame as pg,sys,os,time # Needed to display image/time.
 
-def main():##!!!!!! Need to transmit data over socket in chunks.
+def main():
+	#Sets up socket.
 	s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-	s.connect(('',55555))
-	#s.send('bg')
-	#bps=s.recv(4096)
-	#backgroundPic=pg.image.fromstring(bps[bps.index('QQQQQ')+5:],(int(bps[1:bps.index(',')]),int(bps[bps.index(' ')+1:bps.index(')')])),"RGBA")
+	s.connect(('',55556))
+	#Requests first picture.
+	s.send('bg')
+	#Records number of parts to this picture.
+	num_of_parts=int(s.recv(1024))
+	bps=""
+	#Requests and recieves each part.
+	for i in range(0,num_of_parts):
+		s.send("0")
+		bps+=s.recv(4096)
+	#Decodes image data from assemblage of data. (The first part is the image data, the middle part is the size.)
+	backgroundPic=pg.image.fromstring(bps[bps.index('QQQQQ')+5:],(int(bps[1:bps.index(',')]),int(bps[bps.index(' ')+1:bps.index(')')])),"RGBA")
+	#Repeats process for next image.
 	s.send('pointer')
-	ps=s.recv(12288)
+	num_of_parts=int(s.recv(1024))
+	ps=""
+	for i in range(0,num_of_parts):
+		s.send("hello")
+		ps+=s.recv(4096)
 	pointerPic=pg.image.fromstring(ps[ps.index('QQQQQ')+5:],(int(ps[1:ps.index(',')]),int(ps[ps.index(' ')+1:ps.index(')')])),"RGBA")
 	s.close()
 	#Sets up pygame and displays image/time.
@@ -19,10 +33,9 @@ def main():##!!!!!! Need to transmit data over socket in chunks.
 	mouseP=(0,0)
 	point=(5000,5000)
 	seconds=False
-	#window = pg.display.set_mode((backgroundPic.get_rect().size[0],backgroundPic.get_rect().size[1]))
-	window = pg.display.set_mode((500,500))#DELETE_ME
+	window = pg.display.set_mode((backgroundPic.get_rect().size[0],backgroundPic.get_rect().size[1]))
 	while 1:
-		#window.blit(backgroundPic,(0,0))
+		window.blit(backgroundPic,(0,0))
 		window.blit(pointerPic,mouseP)
 		if seconds:
 			msgObj=font.render("It is {}".format(time.strftime("%H:%M:%S")), 1, pg.Color(0,0,0))
--- getter.py
+++ getter.py
@@ -1,6 +1,5 @@
 import urllib as ul,urllib2 as ul2 # Needed to download image from internet.
 import pygame as pg,socket,os # Needed to load and send image to display.
-import sys#DELETE_ME
 
 def main():
 	print 'Downloading images...'
@@ -18,7 +17,7 @@ def main():
 	
 	#Sets up socket(server) and listens/accepts 1 client.
 	s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-	s.bind(('',55555))
+	s.bind(('',55556))
 	s.listen(1)
 	conn,addr=s.accept()
 	while 1:
@@ -29,11 +28,27 @@ def main():
 			#Sends requested image over socket.
 			if data=='pointer':
 				size=pointer.get_size()
-				conn.send("{}QQQQQ{}".format(size,pg.image.tostring(pointer,"RGBA")))
+				#Puts image data into format readable by the client.
+				sendable="{}QQQQQ{}".format(size,pg.image.tostring(pointer,"RGBA"))
+				#Determines how many portions of data it will have to send to limit maximum size to 4096 per message.
+				numParts=len(sendable)/4096+(len(sendable)%4096!=0)
+				#Sends this info to client to prepare it for reception.
+				conn.send(str(numParts))
+				#Sends each part.
+				for i in range(0,numParts):
+					#Waits for response from client signaling that it is ready.
+					conn.recv(1024)
+					#Sends the next piece of data.
+					conn.send(sendable[i*4096:(i+1)*4096])
 			elif data=='bg':
-				print sys.getsizeof(pg.image.tostring(bg,"RGBA"))
+				#Same as above.
 				size=bg.get_size()
-				conn.send("{}QQQQQ{}".format(size,pg.image.tostring(bg,"RGBA")))
+				sendable="{}QQQQQ{}".format(size,pg.image.tostring(bg,"RGBA"))
+				numParts=len(sendable)/4096+(len(sendable)%4096!=0)
+				conn.send(str(numParts))
+				for i in range(0,numParts):
+					conn.recv(1024)
+					conn.send(sendable[i*4096:(i+1)*4096])
 	conn.close()
 
 if __name__=='__main__':
